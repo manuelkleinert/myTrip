462ef2d4e8b93138772c532f50aaddca639eebef
Manuel Kleinert <info@manuelkleinert.ch>
2019-08-07 17:24:57 +0200

    update mapbox

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
-  'root': true,
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es6': true,
-  },
-  'parserOptions': { 'sourceType': 'module' },
-  'extends': [
-    'eslint:recommended',
-    'airbnb-base'
-  ],
-  'globals': { 'VERSION': true },
+    'root': true,
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true,
+    },
+    'parserOptions': {
+        'sourceType': 'module',
+    },
+    'extends': [
+        'eslint:recommended',
+        'airbnb-base'
+    ],
+    'globals': {
+        'CKEDITOR': true,
+        'VERSION': true,
+        'google': true,
+        'UIkit': true,
+    },
 };
