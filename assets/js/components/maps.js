abcec139c23fbcc0c9810ca070ede42e8813697b
manuelkleinert <manuel.kleinert@gmail.com>
2019-08-07 20:45:18 +0200

    update map box

--- assets/js/components/maps.js
+++ assets/js/components/maps.js
@@ -1,35 +1,41 @@
 import mapBoxGl from 'mapbox-gl';
+import MapBoxGeocoder from 'mapbox-gl-geocoder';
 
 export default {
-    args: 'apiKey',
+  args: 'apiKey',
 
-    props: {
-        apiKey: String,
-        zoom: Number,
-        center: String,
+  props: {
+    apiKey: String,
+    zoom: Number,
+    center: String,
+  },
+
+  data: {
+    apiKey: '',
+    zoom: 12,
+    center: [0, 0],
+  },
+
+  connected() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      mapBoxGl.accessToken = this.apiKey;
+      this.bounds = new mapBoxGl.LngLatBounds();
+
+      this.map = new mapBoxGl.Map({
+        style: 'mapbox://styles/mpk88/cjz1ea4ki0e0j1cmmoi7vxs2v',
+        container: this.$el,
+        center: this.center,
+        zoom: this.zoom,
+      });
+
+      this.map.addControl(new MapBoxGeocoder({
+        accessToken: mapBoxGl.accessToken,
+        mapboxgl: mapBoxGl,
+      }));
     },
-
-    data: {
-        apiKey: '',
-        zoom: 12,
-        center: {lng: 0, lat: 0},
-    },
-
-    connected() {
-        this.initialize();
-    },
-
-    methods: {
-        initialize() {
-            mapBoxGl.accessToken = this.apiKey;
-            this.bounds = new mapBoxGl.LngLatBounds();
-
-            this.map = new mapBoxGl.Map({
-                sprite: 'mapbox://styles/mpk88/cjz1ea4ki0e0j1cmmoi7vxs2v',
-                container: this.$el,
-                center: this.center,
-                zoom: this.zoom,
-            });
-        },
-    },
+  },
 };
